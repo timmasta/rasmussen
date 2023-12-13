6e4543906ec43435dace3de51325c14abda86a59
Tim Mastarone <t.mastarone@vikingelectronics>
2023-12-12 21:53:15 -0600

    Module 06 changes are done.

--- Javascript/dogClass.js
+++ Javascript/dogClass.js
@@ -1,24 +1,7 @@
-/*dogClass.js, Module 04 assignment*/
-/*Gecko	generic	Duckman	Duckman's purple dog; based on characters created by Everett Peck in his Dark Horse publication.*/
+/*dogClass.js, Module 05 assignment*/
+/*3 dogs from TV shows.*/
 
-// Define the object literal
-const dog = {
-    name: 'Gecko',    
-    creator: 'Duckman',
-    mySound: "My real name is Sparky, Duckman stole me and changed my name to Gecko",
-    breed: "Mutt",
 
-    // Method to display information about the character
-    displayInfo: function () {
-        // Using template literals to create a text paragraph
-        const paragraph = `This is the object literal version of the dog:\
-                           \nHi my name is ${this.name}. When I bark I am saying "${this.mySound}".\
-                           \nI starred in the animated TV show Duckman. I have been told by Duckman that my breed is '${this.breed}'.\
-                           \nI was the family dog in Duckman's house in the TV show.`;        
-        console.log(paragraph);
-        alert(paragraph);
-    },
-};
 
 // Define the constructor function
 function Dog(name, creator, mySound, breed, canTalk) {
@@ -28,16 +11,28 @@ function Dog(name, creator, mySound, breed, canTalk) {
     this.breed = breed;
     this.canTalk = canTalk;
 
-    // Method to display information about the character
-    this.displayInfo = function () {
-        // Using template literals to create a text paragraph
-        const paragraph = `Hi my name is ${this.name}. When I bark I am saying "${this.mySound}".\
-                           \nI starred in the animated TV show Duckman. I have been told by Duckman that my breed is '${this.breed}'.\
-                           \nI was the family dog in Duckman's house in the TV show.`;
+    //Method to display each property with a for loop
+    this.displayProperties = function () {
+        // Create a table element
+        document.write("<table border='1'>");
 
-        console.log(paragraph);
-        alert(paragraph);
-    };
+        // Create a table header with the 'name' property
+        let title = this.name;
+        document.write(`<thead><tr><th>${title}</th></tr></thead>`);
+
+        // Iterate over each property and create a table row for it
+        for (let prop in this) {
+            if (this.hasOwnProperty(prop)) {
+                document.write("<tr>");
+                // Create table data cells for property name and value
+                document.write(`<td>${prop}</td><td>${this[prop]}</td>`);
+                document.write("</tr>");
+            }
+        }
+
+        // Close the table element
+        document.write("</table>");
+    };  
 
     // Method to display a greeting which varies based on the boolean argument passed in
     this.myGreeting = function () {
@@ -53,15 +48,16 @@ function Dog(name, creator, mySound, breed, canTalk) {
 
 //Instantiate new Dog and call both methods (from object literal and from the Dog class)
 //This is all in one function so it can be called when the page is loaded
-function createDogInstance(){
+function createDogInstance(name, show, phrase, breed, canTalk){
     // Create an instance of the Dog constructor
-    const myDogConst = new Dog('Gecko', 'Duckman', "My real name is Sparky, Duckman stole me and changed my name to Gecko", 'Mutt', true);
-
-    // Call the displayInfo method
-    myDogConst.displayInfo();
+    const myDogConst = new Dog(name, show, phrase, breed, canTalk);
 
     // Call the myGreeting method
     myDogConst.myGreeting();
-}
 
+   //return the created instance for use elsewhere
+    return myDogConst;
+    
+    
+}
 
